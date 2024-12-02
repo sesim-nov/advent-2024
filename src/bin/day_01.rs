599ab5a008230ff1894bf49b97845788ba08837e
Aaron <aaron@atamisk.net>
2024-12-01 21:57:35 -0500

    day_01 complete.

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,5 +1,6 @@
-use advent_2024::day_01::part_01;
+use advent_2024::day_01::{part_01, part_02};
 
 fn main() {
     part_01("input/01.txt");
+    part_02("input/01.txt");
 }
--- src/day_01.rs
+++ src/day_01.rs
@@ -1,7 +1,14 @@
+use std::collections::HashMap;
+
 use crate::read_file::get_lines;
 
 pub fn part_01(fname: &str) {
     let res = solve_part_01(parse_input(fname));
+    println!("{}", res);    
+}
+
+pub fn part_02(fname: &str) {
+    let res = solve_part_02(parse_input(fname));
     println!("{}", res);    
 }
 
@@ -49,8 +56,17 @@ fn solve_part_01(mut data: ProblemInput) -> usize {
         .sum()
 }
 
-fn solve_part_02(mut data: ProblemInput) -> usize {
-    1
+fn solve_part_02(data: ProblemInput) -> usize {
+    let mut right_map: HashMap<usize, usize> = HashMap::new();
+    for x in data.right {
+        right_map.entry(x).and_modify(|x| *x += 1).or_insert(1);
+    }
+    data.left
+        .iter()
+        .map(|x| -> usize {
+            x * right_map.get(x).unwrap_or(&0)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -70,5 +86,19 @@ mod tests {
 
         //Assert
         assert_eq!(res, 11);
+    }
+    #[test]
+    fn test_solver_02() {
+        //Arrange
+        let test_input = ProblemInput {
+            left:  vec![3, 4, 2, 1, 3, 3],
+            right: vec![4, 3, 5, 3, 9, 3],
+        };
+
+        //Act
+        let res = solve_part_02(test_input);
+
+        //Assert
+        assert_eq!(res, 31);
     }
 }
