09f926657b7cdda0035cd97631bf7bad9f904352
Aaron <1330844+sesim-nov@users.noreply.github.com>
2024-12-04 14:38:21 -0500

    input handling appears to work

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,11 +1,24 @@
 use grid::Grid;
 
+use crate::read_file::get_lines;
+
 pub fn part_01(fname: &str) {
     println!("STUB");
 }
 
 pub fn part_02(fname: &str) {
     println!("STUB");
+}
+
+fn read_input(fname: &str) -> Grid<char>{
+    let mut lines = get_lines(fname);
+    let mut grid_base: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
+    let row_size = grid_base.len();
+    for line in lines {
+        let mut next_row: Vec<char> = line.unwrap().chars().collect();
+        grid_base.append(&mut next_row);
+    }
+    Grid::from_vec(grid_base, row_size)
 }
 
 /// Find the count of times the string XMAS appears in a grid. 
@@ -51,6 +64,21 @@ fn look_for_xmas(pos: (usize, usize), dir: (isize, isize), sequence: &str, g: &G
 mod tests {
     use super::*;
     use grid::grid;
+
+    #[test]
+    fn test_read_input() {
+        //Arrange
+        let fname = "test_input/04.txt";
+        
+        //Act
+        let res = read_input(fname);
+
+        //Assert
+        assert_eq!('M', *res.get(0,0).unwrap());
+        assert_eq!('S', *res.get(1,1).unwrap());
+        assert_eq!('X', *res.get(2,2).unwrap());
+
+    }
 
     #[test]
     fn test_find_xmas() {
