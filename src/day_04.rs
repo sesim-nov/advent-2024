c69d634bacfea3102255d25d1ca0006d1c88305c
Aaron <1330844+sesim-nov@users.noreply.github.com>
2024-12-04 14:17:54 -0500

    Example unit test passing.

--- src/day_04.rs
+++ src/day_04.rs
@@ -10,8 +10,66 @@ pub fn part_02(fname: &str) {
 
 /// Find the count of times the string XMAS appears in a grid. 
 fn find_xmas_count(g: Grid<char>) -> usize {
-    for ((r, c), x) in g.indexed_iter() {
-        println!("{} {} {}", r, c, x);
+    let directions: [(isize, isize); 9] = [(0,-1), (0,0), (0,1), (1,-1), (1,0), (1,1), (-1,-1), (-1,0), (-1,1)];
+    let mut total: usize = 0;
+    for (pos, x) in g.indexed_iter() {
+        if *x == 'X' {
+            total += directions.iter().map(|dir| look_for_xmas(pos, *dir, "X", &g)).sum::<usize>();
+        }
     }
-    4
+    total
+}
+
+fn look_for_xmas(pos: (usize, usize), dir: (isize, isize), sequence: &str, g: &Grid<char>) -> usize {
+    let mut sequence = String::from(sequence);
+    let mut curr_row = pos.0;
+    let mut curr_col = pos.1;
+    loop {
+        curr_row = match curr_row.checked_add_signed(dir.0){
+            Some(e) => e,
+            None => {break 0}
+        };
+        curr_col = match curr_col.checked_add_signed(dir.1){
+            Some(e) => e,
+            None => {break 0}
+        };
+        match g.get(curr_row, curr_col) {
+            None => {break 0},
+            Some(ch) => sequence.push(*ch),
+        }
+        match sequence.as_str() {
+            "X" => continue,
+            "XM" => continue,
+            "XMA" => continue,
+            "XMAS" => break 1,
+            _ => break 0
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use grid::grid;
+
+    #[test]
+    fn test_find_xmas() {
+        //Arrange
+        let test_grid = grid![
+            ['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M']
+            ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A']
+            ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M']
+            ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X']
+            ['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M']
+            ['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A']
+            ['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S']
+            ['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A']
+            ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M']
+            ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']];
+        //Act
+        let res = find_xmas_count(test_grid);
+
+        //Assert
+        assert_eq!(res, 18);
+    }
 }
