7a84d78711f0550c00b3726fe74b2a8ef229b625
Aaron <1330844+sesim-nov@users.noreply.github.com>
2024-12-04 08:22:57 -0500

    drafting out core fn for doy 04

--- src/day_04.rs
+++ src/day_04.rs
@@ -7,3 +7,11 @@ pub fn part_01(fname: &str) {
 pub fn part_02(fname: &str) {
     println!("STUB");
 }
+
+/// Find the count of times the string XMAS appears in a grid. 
+fn find_xmas_count(g: Grid<char>) -> usize {
+    for ((r, c), x) in g.indexed_iter() {
+        println!("{} {} {}", r, c, x);
+    }
+    4
+}
