ba0ee2b4bbd63c6d90136fa84322c16082421b70
Aaron <1330844+sesim-nov@users.noreply.github.com>
2025-01-06 17:10:20 -0500

    working on parsing

--- src/day_08.rs
+++ src/day_08.rs
@@ -1,7 +1,40 @@
+use std::collections::HashMap;
+
+use crate::read_file::get_lines;
+
 pub fn part_01(fname: &str) {
     println!("STUB");
 }
 
 pub fn part_02(fname: &str) {
     println!("STUB");
+}
+
+fn parse_input(fname: &str) {
+    let lines = get_lines(fname);
+    let mut row_count = 0;
+    let mut col_count = None;
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for (i, line) in lines.enumerate() {
+        row_count = i;
+        let chars: Vec<char> = line.unwrap().chars().collect();
+        if let None = col_count {
+            col_count = Some(chars.len());
+        }
+        for (j,c) in chars.iter().enumerate(){
+            match c {
+                '.' => (),
+                c => {
+                    match antennas.get_mut(c) {
+                        Some(v) => {
+                            v.push((i, j));
+                        }
+                        None => {
+                            antennas.insert(*c, vec![(i, j)]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
