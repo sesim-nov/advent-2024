b1c48dcb302d2495b98110b5ea1c5b82c1041862
Aaron <1330844+sesim-nov@users.noreply.github.com>
2025-01-06 16:01:48 -0500

    Day 08 start

---
+++ src/bin/day_08.rs
@@ -0,0 +1,6 @@
+use advent_2024::day_08::{part_01, part_02};
+
+fn main() {
+    part_01("input/07.txt");
+    part_02("input/07.txt");
+}
---
+++ src/day_08.rs
@@ -0,0 +1,7 @@
+pub fn part_01(fname: &str) {
+    println!("STUB");
+}
+
+pub fn part_02(fname: &str) {
+    println!("STUB");
+}
